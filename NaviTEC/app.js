390ba3c9296368ecc24cdcb129646220fa5740fb
Jose Blanco <joseblanco1313@gmail.com>
2020-12-22 21:20:13 -0600

    JS and HTML trial

--- NaviTEC/app.js
+++ NaviTEC/app.js
@@ -1,11 +1,11 @@
-var mysql = require ('mysql');
 
+var mysql = require ('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     database: 'navitec',
     user: 'root',
     /* Add password*/
-    password: ''
+    password: '260301'
 });
 
 connection.connect(function (error){
@@ -14,14 +14,37 @@ if (error){
 } else {
     console.log ("Excelent connection")
 }
+
 });
 
+function SAselect () {
+    var SATable = "";
+    var query = 'SELECT * FROM santas_assistant';
+    connection.query(query, function (err,results){
+        if (err) throw err;
+            console.log (results);
+            for (count =0; count < results.length; count++){
+                SATable += '<tr>';
+                SATable += '  <td>' + results[count].assistant_id + '</td>';
+                SATable += '  <td>' + results[count].name + '</td>';
+                SATable += '  <td>' + results[count].start_date + '</td>';
+                SATable += '  <td>' + results[count].username + '</td>';
+                SATable += '</tr>';
+            }
+            document.getElementById ('SATable').innerHTML = SATable;
+    });
+    connection.end(() => { });
+};
+SAselect();
+
+
+
+/*
 connection.query ('SELECT * FROM santas_assistant', function (error, results, fields){
 if (error)
 throw error;
 results.forEach (result => {
-    console.log (result);
+    console.log (result); 
 });
-});
-
-connection.end();
+*/
+/*connection.end();*/
